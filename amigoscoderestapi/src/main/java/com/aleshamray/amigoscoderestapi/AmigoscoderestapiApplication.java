4a0b1f9674492a8cebcaf2bf735ec17a23fe9980
Alesha Ray <aray@ARayWorcBookPro.attlocal.net>
2022-06-12 18:14:10 -0500

    initial commit

---
+++ amigoscoderestapi/src/main/java/com/aleshamray/amigoscoderestapi/AmigoscoderestapiApplication.java
@@ -0,0 +1,13 @@
+package com.aleshamray.amigoscoderestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoscoderestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigoscoderestapiApplication.class, args);
+	}
+
+}
