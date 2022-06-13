e63b061702603b7787b0c93a9a458579ba880d12
Alesha Ray <alesha.ray@g.austincc.edu>
2022-06-12 20:31:01 -0500

    add student controller and services

---
+++ amigoscoderestapi/src/main/java/com/aleshamray/amigoscoderestapi/service/StudentService.java
@@ -0,0 +1,20 @@
+package com.aleshamray.amigoscoderestapi.service;
+
+import com.aleshamray.amigoscoderestapi.domain.Student;
+import com.aleshamray.amigoscoderestapi.repository.StudentRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+}
