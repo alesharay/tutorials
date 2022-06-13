82d5d3596b83a0f2eb8f01eaaaaf2dae0d94cd60
Alesha Ray <aray@ARayWorcBookPro.attlocal.net>
2022-06-12 20:18:01 -0500

    did a bunch of things

--- amigoscoderestapi/src/main/java/com/aleshamray/amigoscoderestapi/domain/Student.java
+++ amigoscoderestapi/src/main/java/com/aleshamray/amigoscoderestapi/domain/Student.java
@@ -3,9 +3,11 @@ package com.aleshamray.amigoscoderestapi.domain;
 import com.aleshamray.amigoscoderestapi.enums.Gender;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.util.List;
 
@@ -16,10 +18,29 @@ public class Student {
     private String id;
     private String firstName;
     private String lastName;
+    @Indexed(unique = true)
     private String email;
     private Gender gender;
     private Address address;
     private List<String> favoriteSubjects;
     private BigDecimal totalSpentInBooks;
-    private ZonedDateTime createdAt;
+    private LocalDateTime createdAt;
+
+    public Student(String firstName,
+                   String lastName,
+                   String email,
+                   Gender gender,
+                   Address address,
+                   List<String> favoriteSubjects,
+                   BigDecimal totalSpentInBooks,
+                   LocalDateTime createdAt) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+        this.address = address;
+        this.favoriteSubjects = favoriteSubjects;
+        this.totalSpentInBooks = totalSpentInBooks;
+        this.createdAt = createdAt;
+    }
 }
