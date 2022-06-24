c4ffb35adb79c10c06c0d0262e69ee1105f1b562
Alesha Ray <aray@alesharcbookpro.attlocal.net.us.qdx.com>
2022-06-24 08:49:12 -0500

    rest api with spring and vue

---
+++ mahesh-spring-vue/maheshrestapi/src/test/java/com/aleshamray/maheshrestapi/MaheshrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleshamray.maheshrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaheshrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
